aa8bc957459f883a3521bdc640461d53237cee6a
lyfaradison-jpg <lyfaradison@gmail.com>
2026-06-21 22:27:20 +0200

    Update service-worker.js
    Fgg

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,1 @@
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open('clipmind-v1').then(cache => {
-      return cache.addAll([
-        '/',
-        '/index.html'
-      ]);
-    })
-  );
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
-});if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('./service-worker.js');
-}
+ff
